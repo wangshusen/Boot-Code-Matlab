fc88cb58ada15f9ba65810517633a87d94c967b2
Shusen Wang <wssatzju@gmail.com>
2017-06-26 16:04:11 -0700

    initial commit

--- batch1.m
+++ batch1.m
@@ -1,33 +1,6 @@
 setup
 
-infile = './data/connect.mat';
-experimentAA(infile, 'sampling');
-experimentAA(infile, 'gaussian');
-experimentAA(infile, 'srht');
-
-
-infile = './data/dna.mat';
-experimentAA(infile, 'sampling');
-experimentAA(infile, 'gaussian');
-experimentAA(infile, 'srht');
-
-
-
-infile = './data/mushrooms.mat';
-experimentAA(infile, 'sampling');
-experimentAA(infile, 'gaussian');
-experimentAA(infile, 'srht');
-
-
-
-infile = './data/protein.mat';
-experimentAA(infile, 'sampling');
-experimentAA(infile, 'gaussian');
-experimentAA(infile, 'srht');
-
-
-
-infile = './data/mnist.mat';
+infile = 'mnist.mat';
 experimentAA(infile, 'sampling');
 experimentAA(infile, 'gaussian');
 experimentAA(infile, 'srht');
---
+++ batch2.m
@@ -0,0 +1,29 @@
+setup
+
+
+infile = 'dna.mat';
+experimentAA(infile, 'sampling');
+experimentAA(infile, 'gaussian');
+experimentAA(infile, 'srht');
+
+infile = 'connect.mat';
+experimentAA(infile, 'sampling');
+experimentAA(infile, 'gaussian');
+experimentAA(infile, 'srht');
+
+
+
+infile = 'mushrooms.mat';
+experimentAA(infile, 'sampling');
+experimentAA(infile, 'gaussian');
+experimentAA(infile, 'srht');
+
+
+
+infile = 'protein.mat';
+experimentAA(infile, 'sampling');
+experimentAA(infile, 'gaussian');
+experimentAA(infile, 'srht');
+
+
+
